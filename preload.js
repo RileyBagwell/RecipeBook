7ddbe13a16a2fef2baeb1bfac1702fd4181aa7fa
Harthroth <38116988+Harthroth@users.noreply.github.com>
2023-11-11 15:49:04 -0600

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const leNodeVersion = document.getElementById('node-version');
-    leNodeVersion.innerText = process.versions['node'];
-
-    document.getElementById('chrome-version').innerText = process.versions['chrome'];
-    document.getElementById('electron-version').innerText = process.versions['electron'];
-});
